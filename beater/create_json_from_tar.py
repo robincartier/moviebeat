b8fff80525fc89968b995d61ce7a6cc8b9c577e5
Robin Cartier <robin.cartier2@gmail.com>
2018-10-29 16:56:13 +0100

    index fields and cleanup

--- beater/create_json_from_tar.py
+++
@@ -1,9 +0,0 @@
-import os, json
-
-# os.system("wget https://datasets.imdbws.com/title.basics.tsv.gz")
-# os.system("gunzip title.basics.tsv.gz")
-
-# get the last read line of the IMDB file
-f = open('beater/last_line_imdb.txt','r+')
-last_line = int(f.read())
-print(last_line)
